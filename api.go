3a5362a301cec5f5f8e5b7b90fc66a1bbf1b5d80
Subhash Popuri <pbssubhash@gmail.com>
2020-08-11 13:17:38 +0530

    Initial Commit

---
+++ api.go
@@ -0,0 +1,49 @@
+package Shodan
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type Credential struct {
+	Url string
+	Key string
+}
+
+type Response struct {
+	Message string
+	err     bool
+}
+
+type APIInfo struct {
+	QueryCredits int    `json:"query_credits"`
+	ScanCredits  int    `json:"scan_credits"`
+	Telnet       bool   `json:"telner"`
+	Plan         string `json:"plan"`
+	Https        bool   `json:"https"`
+	Unlocked     bool   `json:"unlocked"`
+}
+
+func (cred *Credential) Init() *Response {
+	res, err := http.Get(fmt.Sprintf("%s/api-info?key=%s", cred.Url, cred.Key))
+	body, err := ioutil.ReadAll(res.Body)
+	var tesl APIInfo
+	json.Unmarshal([]byte(body), &tesl)
+	if tesl.Unlocked == true {
+		fmt.Print("Unlocked")
+	}
+	if err != nil {
+		return &(Response{"Success", false})
+	}
+	return &(Response{"Success", false})
+}
+
+func Setup(apikey string, url string) *Response {
+	var Creda Credential
+	Creda = Credential{Url: url, Key: apikey}
+	resp := Creda.Init()
+	return resp
+
+}
---
+++ cmd/shodan/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("[+] Welcome to Shodan GO Client [-]")
+	fmt.Println("[-] Built by zer0 p1k4chu for learning purposes. [-]")
+	shodan.Setup("")
+}
